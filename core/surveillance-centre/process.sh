028d922c5681ff9cbbf255efd9a479d8c4f4e175
Shebin Jose Jacob <shebinjosejacob@gmail.com>
2020-08-09 16:55:22 +0530

    Update process.sh

--- core/surveillance-centre/process.sh
+++ core/surveillance-centre/process.sh
@@ -2,6 +2,8 @@
 now=$(date +"%R")
 #Assuming that you have the folder surveillance-centre in your Documents folder
 alpr -c in /home/user/Documents/surveillance-centre/Demo/car.mp4 >> /home/user/Documents/surveillance-centre/temp/temp0.txt
+#For using webcam
+#alpr -c in webcam >> /home/user/Documents/surveillance-centre/temp/temp0.txt
 grep "confidence" /home/user/Documents/surveillance-centre/temp/temp0.txt | sort -k4 > /home/user/Documents/surveillance-centre/temp/temp1.txt
 awk '{print $2}' /home/user/Documents/surveillance-centre/temp/temp1.txt> /home/user/Documents/surveillance-centre/log/$now.txt
 echo $now>>/home/user/Documents/surveillance-centre/log/$now.txt
