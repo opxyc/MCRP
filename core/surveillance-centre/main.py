4cf3c420286f53d937045a1577ad7c35e10a31a2
Shebin Jose Jacob <shebinjosejacob@gmail.com>
2020-08-09 15:06:51 +0530

    Update main.py

--- core/surveillance-centre/main.py
+++ core/surveillance-centre/main.py
@@ -1,9 +1,9 @@
 from crontab import CronTab
-
+#Assuming that you have the folder 'surveillance-centre' in your Documents folder
 my_cron = CronTab(user='user')
-job1 = my_cron.new(command='cd /home/user/Documents/ && ./process.sh')
+job1 = my_cron.new(command='cd /home/user/surveillance-centre/Documents/ && ./process.sh')
 job1.minute.every(2)
-job2 = my_cron.new(command='cd /home/user/Documents/ && python3 match.py')
+job2 = my_cron.new(command='cd /home/user/surveillance-centre/Documents/ && python3 match.py')
 job2.minute.every(2)
 
 my_cron.write()
